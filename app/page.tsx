38abf09dc5ec6ca76978316e38e37e7bece55a89
Mohamed Akeel <akeelaslamcse@gmail.com>
2024-11-08 01:47:00 +0530

    Assets & Meta

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <h1 className="h2-bold background-light850_dark100">
-      Welcome to the World of Next JS
-    </h1>
+    <>
+      <h1 className="h2-bold ">Welcome to the World of Next JS</h1>
+      <h1 className="h2-bold font-space-grotesk text-primary-500">
+        Welcome to the World of Next JS
+      </h1>
+    </>
   );
 }
